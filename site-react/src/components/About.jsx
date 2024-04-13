3676f3024b048957af7dd6d6769d198bf77189b9
Kagiso Monageng <kagiso.monageng@bastiongroup.co.za>
2024-04-13 16:03:32 +0200

    anchors

--- site-react/src/components/About.jsx
+++ site-react/src/components/About.jsx
@@ -33,9 +33,9 @@ export default function About() {
 
   return (
     <>
-      <div className="container py-96 overflow-hidden">
-        <h2 className='font-bold text-2xl mb-7 bebas abt-heading text-center'>About</h2>
-
+      <div className="container py-96 overflow-hidden mx-auto" >
+        <h2 className='font-bold text-2xl mb-7 bebas abt-heading text-center' id='about'>About</h2>
+     
 
         <div className="w-full lg:w-1/2 mx-auto">
           <p className='about-text  mb-4 text-center'>
@@ -43,21 +43,19 @@ export default function About() {
 
           <p className='about-text  text-center'>He started singing from an early age and would frequently enter scholar music competitions. KAGIISO’s sound has been greatly influenced and shaped by the likes of John Legend, Langa Mavuso, Manana, Sam Smith and Donald. </p>
 
-          <div class="indicator w-full my-5 about-text">
-            <span class="indicator-item badge  badge-outline text-xs">KAGIISO</span>
-            <div class=" w-full bg-base-300 place-items-center py-8 px-3 italic"><p className='text-center'>“The songs of these artists make one believe that love really exists. I am marvelled at how they create beautiful well composed music. This is what I want people to feel when they listen to my music.”</p></div>
+          <div className="indicator w-full my-5 about-text">
+            <span className="indicator-item badge badge-outline text-xs">KAGIISO</span>
+            <div className=" w-full bg-base-300 place-items-center py-8 px-3 italic"><p className='text-center'>“The songs of these artists make one believe that love really exists. I am marvelled at how they create beautiful well composed music. This is what I want people to feel when they listen to my music.”</p></div>
           </div>
 
           <p className='about-text  text-center'>KAGIISO has had the honour to work with South African RnB/House legend Donald alongside the Dube brothers, King Khustah and Ditheto Music.</p>
 
-          <div class="indicator w-full my-5 about-text">
-            <span class="indicator-item badge badge-outline text-xs">KAGIISO</span>
-            <div class=" w-full bg-base-300 place-items-center py-8 px-3 italic"><p className='text-center'> ⁠”Music evokes memories and emotions. It can touch hearts and shape them in a positive way.”
+          <div className="indicator w-full my-5 about-text">
+            <span className="indicator-item badge badge-outline text-xs">KAGIISO</span>
+            <div className=" w-full bg-base-300 place-items-center py-8 px-3 italic"><p className='text-center'> ⁠”Music evokes memories and emotions. It can touch hearts and shape them in a positive way.”
             </p></div>
           </div>
         </div>
-
-
 
       </div>
     </>
--- site-react/src/components/Navigation.jsx
+++ site-react/src/components/Navigation.jsx
@@ -1,21 +1,64 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { gsap } from "gsap";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+gsap.registerPlugin(ScrollToPlugin);
 
 export default function Navigation() {
+
+    useEffect(() => {
+        const anchorLinks = document.querySelectorAll('.scrollTo');
+
+        anchorLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                console.log("clicked");
+                let target = "#" + link.rel;
+                gsap.to(window, { duration: 2, scrollTo: target });
+            })
+        });
+    }, [])
+
+
     return (
-        <div className="w-full absolute top-0 py-8 z-50 flex justify-center">
-            <div className="container">
-                <div className="flex gap-8 place-items-center justify-center">
-                    <ul className="menu menu-horizontal lg:gap-8">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                    </ul>
-                    <span className="logo font-bold text-3xl bebas">KAGIISO</span>
-                    <ul className="menu menu-horizontal lg:gap-8">
-                        <li><a href="#">Discography</a></li>
-                        <li><a href="#">Bookings</a></li>
+        <div className="w-full absolute top-0  z-50 flex justify-center">
+            <div className="drawer">
+                <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex flex-col">
+                    {/* Navbar */}
+                    <div className="w-full navbar place-items-center">
+
+                        <span className="flex-1 logo text-2xl font-semibold bebas">KAGIISO</span>
+                        <div className="flex-none lg:hidden">
+                            <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                            </label>
+                        </div>
+                        <div className="flex-none hidden lg:block">
+                            <ul className="menu menu-horizontal place-items-center">
+                                {/* Navbar menu content here */}
+                                <li><a className="scrollTo" rel='home'>Home</a></li>
+                                <li><a className="scrollTo" rel='about'>About</a></li>
+                                <li><a className="scrollTo" rel='discography'>Discography</a></li>
+                                <li><a className='scrollTo btn btn-outline' rel='bookings'>Bookings</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    {/* Page content here */}
+
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-80 min-h-full bg-base-200 relative">
+                        {/* Sidebar content here */}
+                        <li><a className="scrollTo" rel='home'>Home</a></li>
+                        <li><a className="scrollTo" rel='about'>About</a></li>
+                        <li><a className="scrollTo" rel='discography'>Discography</a></li>
+                        <li><a className='scrollTo btn btn-outline' rel='bookings'>Bookings</a></li>
+                        <li className="absolute bottom-4 left-0">
+                            <div className="py-1 text-xs">WORDS  &ndash;  EP is out now.<br></br>Available on all major streaming platforms</div>
+                        </li>
                     </ul>
                 </div>
-
             </div>
         </div>
     )
